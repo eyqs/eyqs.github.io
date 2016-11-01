6a57ed95a2402ed8a58de7e7d541cd59826c9185
eyqs <eugene@eyqs.ca>
2016-11-01 12:22:10 -0700

    Add temporary 2016 October blog post

--- archive.py
+++ archive.py
@@ -4,7 +4,8 @@ pages = ['', 'about/', 'blog/', 'cakes/', 'documents/', 'projects/',
          'questions/', 'resume/', 'sitemap/', 'tools/']
 posts = ['1601-first-post/', '1602-dwarf-fort/', '1603-teach-aide/',
          '1604-hard-ware/', '1605-may-exams/', '1606-elap-rules/',
-         '1607-intro-books/', '1608-first-year/']
+         '1607-intro-books/', '1608-first-year/', '1609-lack-code/',
+         '1610-net-hack/']
 if __name__ == '__main__':
     for page in pages:
         urllib.request.urlopen('http://web.archive.org/save/' +
