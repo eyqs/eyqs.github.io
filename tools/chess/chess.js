36bbe7db0f089ee11a818dcca6387a8aa79fc7ab
eyqs <eugene@eyqs.ca>
2017-08-12 01:00:51 -0700

    Update chess

--- tools/chess/chess.js
+++ tools/chess/chess.js
@@ -18,12 +18,11 @@
 
 // global variables
 
-let board;
+let state;                          // state of a board at a given turn;
+let history;                        // history of all states of the board
+let white_turn;                     // true = white, false = black
 let selection;                      // ex. {piece: "P", row: 3, col: 5}
 let error;                          // ex. {row: 0, col: 7}
-let white_turn;                     // true = white, false = black
-let en_passant;                     // column at which en passant can occur
-let can_castle;                     // ex. {0: {1: true, 5: false}, 7: ...}
 
 
 // constants
@@ -32,7 +31,8 @@ const ROWS = 8;
 const COLS = 8;
 const WHITE_PIECES = "PNBRQK";
 const BLACK_PIECES = "pnbrqk";
-const INITIAL_CASTLE = {0: {1: true, 5: true}, 7: {1: true, 5: true}};
+const INITIAL_CASTLE = {0: {0: true, 7: true}, 7: {0: true, 7: true}};
+const FINAL_CASTLE = {0: {0: false, 7: false}, 7: {0: false, 7: false}};
 const INITIAL_BOARD = [
   "R", "N", "B", "K", "Q", "B", "N", "R",
   "P", "P", "P", "P", "P", "P", "P", "P",
@@ -43,6 +43,12 @@ const INITIAL_BOARD = [
   "p", "p", "p", "p", "p", "p", "p", "p",
   "r", "n", "b", "k", "q", "b", "n", "r",
 ];
+const INITIAL_STATE = {
+  board: INITIAL_BOARD,
+  current_turn: 0,
+  en_passant_column: -1,
+  can_castle: INITIAL_CASTLE,
+};
 
 
 // convert row and col to cell number
@@ -88,6 +94,31 @@ function getIcon(piece) {
 }
 
 
+/* return true iff the player can castle with the rook in the given position
+ * requires:
+ *  - 0 <= selection.row, row < ROWS
+ *  - 0 <= selection.col, col < COLS
+ */
+
+function canCastle(selection, row, col) {
+  let rook_col;
+  if (col === 1) {
+    rook_col = 0;
+  } else if (col === 5) {
+    rook_col = 7;
+  } else {
+    return false;
+  }
+  if (!isEmpty("orthogonal", selection.row, selection.col, row, rook_col)) {
+    return false;
+  }
+  if (row !== 0 && row !== 7) {
+    return false;
+  }
+  return state.can_castle[row][rook_col];
+}
+
+
 /* return true iff all the squares between start and end exclusive are empty
  *  - e.g. to move a bishop from (0,0) to (2,2), only (1,1) must be empty
  * requires:
@@ -102,14 +133,14 @@ function isEmpty(direction, start_row, start_col, end_row, end_col) {
     if (start_row === end_row) {
       if (start_col < end_col) {
         for (let col = start_col + 1; col < end_col; col++) {
-          if (board[toCell(start_row, col)]) {
+          if (state.board[toCell(start_row, col)]) {
             return false;
           }
         }
         return true;
       } else if (start_col > end_col) {
         for (let col = start_col - 1; col > end_col; col--) {
-          if (board[toCell(start_row, col)]) {
+          if (state.board[toCell(start_row, col)]) {
             return false;
           }
         }
@@ -118,14 +149,14 @@ function isEmpty(direction, start_row, start_col, end_row, end_col) {
     } else if (start_col === end_col) {
       if (start_row < end_row) {
         for (let row = start_row + 1; row < end_row; row++) {
-          if (board[toCell(row, start_col)]) {
+          if (state.board[toCell(row, start_col)]) {
             return false;
           }
         }
         return true;
       } else if (start_row > end_row) {
         for (let row = start_row - 1; row > end_row; row--) {
-          if (board[toCell(row, start_col)]) {
+          if (state.board[toCell(row, start_col)]) {
             return false;
           }
         }
@@ -138,15 +169,15 @@ function isEmpty(direction, start_row, start_col, end_row, end_col) {
       if (start_row < end_row) {
         for (let row = start_row + 1, col = start_col + 1;
             row < end_row && col < end_col; row++, col++) {
-          if (board[toCell(row, col)]) {
+          if (state.board[toCell(row, col)]) {
             return false;
           }
         }
         return true;
       } else if (start_row > end_row) {
-        for (let row = start_row - 1, col = start_col + 1;
-            row > end_row && col < end_col; row--, col++) {
-          if (board[toCell(row, col)]) {
+        for (let row = start_row - 1, col = start_col - 1;
+            row > end_row && col > end_col; row--, col--) {
+          if (state.board[toCell(row, col)]) {
             return false;
           }
         }
@@ -156,15 +187,15 @@ function isEmpty(direction, start_row, start_col, end_row, end_col) {
       if (start_row < end_row) {
         for (let row = start_row + 1, col = start_col - 1;
             row < end_row && col > end_col; row++, col--) {
-          if (board[toCell(row, col)]) {
+          if (state.board[toCell(row, col)]) {
             return false;
           }
         }
         return true;
       } else if (start_row > end_row) {
-        for (let row = start_row - 1, col = start_col - 1;
-            row > end_row && col > end_col; row--, col--) {
-          if (board[toCell(row, col)]) {
+        for (let row = start_row - 1, col = start_col + 1;
+            row > end_row && col < end_col; row--, col++) {
+          if (state.board[toCell(row, col)]) {
             return false;
           }
         }
@@ -204,7 +235,9 @@ function canMove(selection, row, col) {
       // capturing move
       return ((white_turn && row === selection.row + 1)
               || (!white_turn && row === selection.row - 1))
-          && (board[toCell(row, col)] || en_passant === col);
+          && (state.board[toCell(row, col)]
+              || (((white_turn && row === 5) || (!white_turn && row === 2))
+                  && col === state.en_passant_column));
     }
     break;
   case "n":
@@ -229,7 +262,7 @@ function canMove(selection, row, col) {
         && (row === selection.row + 1 || row === selection.row
             || row === selection.row - 1))
     // castling, TODO: check
-        || (row === selection.row && can_castle[row][col]
+        || (row === selection.row && canCastle(selection, row, col)
             && (col === selection.col + 2 || col === selection.col - 2));
     break;
   }
@@ -239,74 +272,70 @@ function canMove(selection, row, col) {
 
 /* move the selected piece to the given cell
  * requires: the move is already validated by canMove
- * modifies: board, en_passant, can_castle
+ * modifies: state
  */
 
 function doMove(selection, row, col) {
   const piece = selection.piece.toLowerCase();
-  board[toCell(row, col)] = selection.piece;
-  board[toCell(selection.row, selection.col)] = "";
+  state.board[toCell(row, col)] = selection.piece;
+  state.board[toCell(selection.row, selection.col)] = "";
 
   // en passant
-  if (piece === "p" && en_passant === col && col !== selection.col) {
+  if (piece === "p" && col !== selection.col
+      && ((white_turn && row === 5) || (!white_turn && row === 2))
+      && state.en_passant_column === col) {
     if (white_turn) {
-      board[toCell(row - 1, col)] = "aou";
+      state.board[toCell(row - 1, col)] = "";
     } else {
-      board[toCell(row + 1, col)] = "aou";
+      state.board[toCell(row + 1, col)] = "";
     }
   }
   if (piece === "p"
       && (row === selection.row + 2 || row === selection.row - 2)) {
-    en_passant = col;
+    state.en_passant_column = col;
   } else {
-    en_passant = -1;
+    state.en_passant_column = -1;
   }
 
   // castling
   if (piece === "k") {
     if (col === selection.col + 2) {
-      board[toCell(row, col - 1)] = board[toCell(row, COLS - 1)];
-      board[toCell(row, COLS - 1)] = "";
+      state.board[toCell(row, col - 1)] = state.board[toCell(row, COLS - 1)];
+      state.board[toCell(row, COLS - 1)] = "";
     } else if (col === selection.col - 2) {
-      board[toCell(row, col + 1)] = board[toCell(row, 0)];
-      board[toCell(row, 0)] = "";
+      state.board[toCell(row, col + 1)] = state.board[toCell(row, 0)];
+      state.board[toCell(row, 0)] = "";
     } else {
-      can_castle[0][1] = false;
-      can_castle[0][5] = false;
-      can_castle[7][1] = false;
-      can_castle[7][5] = false;
+      state.can_castle = JSON.parse(JSON.stringify(FINAL_CASTLE));
     }
   }
   if (piece === "r" && (selection.row === 0 || selection.row === 7)) {
-    if (selection.col === 0) {
-      can_castle[selection.row][1] = false;
-    } else if (selection.col === 7) {
-      can_castle[selection.row][5] = false;
-    }
+    state.can_castle[selection.row][selection.col] = false;
   }
 }
 
 
 /* decide what to do when user clicks
  * requires: 0 <= row < ROWS, 0 <= col < COLS
- * modifies: selection, white_turn, calls doMove
+ * modifies: state, selection, error
  */
 
 function clickCell(row, col) {
   const cell = toCell(row, col);
-  const piece = board[cell];
+  const piece = state.board[cell];
   if (selection) {
     if (selection.row === row && selection.col === col) {
-      error = null;
       selection = null;
-    } else if (canMove(selection, row, col)) {
-      if (!piece || (white_turn && WHITE_PIECES.indexOf(piece) === -1)
-          || (!white_turn && BLACK_PIECES.indexOf(piece) === -1)) {
-        white_turn = !white_turn;
-        doMove(selection, row, col);
-        error = null;
-        selection = null;
-      }
+      error = null;
+    } else if ((!piece || (white_turn && WHITE_PIECES.indexOf(piece) === -1)
+            || (!white_turn && BLACK_PIECES.indexOf(piece) === -1))
+        && canMove(selection, row, col)) {
+      doMove(selection, row, col);
+      selection = null;
+      error = null;
+      state.current_turn++;
+      history.push(JSON.stringify(state));
+      white_turn = !(state.current_turn % 2);
     } else {
       error = {row, col};
     }
@@ -321,6 +350,17 @@ function clickCell(row, col) {
 }
 
 
+// restore the board to its state at the given turn
+
+function restoreBoard(turn) {
+  history.splice(turn + 1);
+  state = JSON.parse(history[turn]);
+  white_turn = !(turn % 2);
+  selection = null;
+  render();
+}
+
+
 // render the board
 
 function render() {
@@ -329,7 +369,7 @@ function render() {
   for (let row = 0; row < ROWS; row++) {
     html.push("<div class='row'>\n");
     for (let col = 0; col < COLS; col++) {
-      const piece = board[toCell(row, col)];
+      const piece = state.board[toCell(row, col)];
       html.push(
 				`  <div class="${getClass(piece, row, col)}">
 					   <div class="piece">${getIcon(piece)}</div>
@@ -351,15 +391,21 @@ function render() {
 }
 
 
+// undo the previous move
+
+function undoMove() {
+  restoreBoard(state.current_turn - 1);
+}
+
+
 // start the game
 
 function startGame() {
-  board = JSON.parse(JSON.stringify(INITIAL_BOARD));
+  state = JSON.parse(JSON.stringify(INITIAL_STATE));
+  history = [JSON.stringify(state)];
+  white_turn = true;
   selection = null;
   error = null;
-  white_turn = true;
-  en_passant = -1;
-  can_castle = JSON.parse(JSON.stringify(INITIAL_CASTLE));
   render();
 }
 
@@ -368,11 +414,5 @@ function startGame() {
 
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("start").addEventListener("click", startGame);
+  document.getElementById("undo").addEventListener("click", undoMove);
 });
-
-
-// register service worker
-
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("worker.js");
-}
