e9c6b0944b02f4e92ab0f0a985b91db082c3ff05
Eugene Y. Q. Shen <eugene@eyqs.ca>
2018-11-30 23:58:18 -0800

    Add 2018 November blog post

--- archive.py
+++ archive.py
@@ -3,7 +3,8 @@ import urllib.request
 pages = ['', 'about/', 'blog/', 'cakes/', 'documents/', 'projects/',
          'questions/', 'resume/', 'sitemap/', 'tools/']
 posts = ['1804-ubcs-cpen/', '1805-late-rides/', '1806-wtfjs-idkjs/',
-         '1807-cloud-next/', '1808-sunny-vale/', '1809-mug-share/']
+         '1807-cloud-next/', '1808-sunny-vale/', '1809-mug-share/',
+         '1811-music-18w1/']
 if __name__ == '__main__':
     for page in pages:
         print('Archiving page: ' + page)
